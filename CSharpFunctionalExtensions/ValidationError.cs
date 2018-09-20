e07cd3ee3ae0267d02fd694c12d9bcb5e45c8b79
Chad Gilbert <chad.gilbert@cqlcorp.com>
2018-09-19 21:40:41 -0400

    Add initial ValidationError struct

---
+++ CSharpFunctionalExtensions/ValidationError.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Serialization;
+
+namespace CSharpFunctionalExtensions
+{
+    public struct ValidationError : ISerializable
+    {
+        private readonly string field;
+        private readonly string error;
+
+        public ValidationError(string error) {
+            field = string.Empty;
+            this.error = error ?? string.Empty;
+        }
+
+        public ValidationError(string field, string error) {
+            this.field = field ?? string.Empty;
+            this.error = error ?? string.Empty;
+        }
+
+        void ISerializable.GetObjectData(SerializationInfo oInfo, StreamingContext oContext)
+        {
+            oInfo.AddValue("Field", Field);
+            oInfo.AddValue("Error", Error);
+        }
+
+        public string Field { get { return field ?? string.Empty; } }
+        public string Error { get { return error ?? string.Empty; } }
+    }
+}
